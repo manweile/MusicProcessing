0ef4f87da5ebbc008188765eed789fd9480bb5c7
Gerald Manweiler <manweile@ualberta.ca>
2025-04-28 19:47:31 -0600

    Update __init__.py

--- src/dir_processing/__init__.py
+++ src/dir_processing/__init__.py
@@ -1,15 +1,14 @@
 '''
 @package dir_processing
 @module directory_processing
-@todo package documentation
 '''
 
-## either relative or absolute import work
+# either relative or absolute import work
 # relative import
 from .. dir_processing.directory_processing import DirectoryProcessing
 # absolute import
 # from src.dir_processing.directory_processing import DirectoryProcessing
 
 ## @var __all__
-# @brief expose class for imports
+# @brief exposes list for imports
 __all__ = ['DirectoryProcessing']
