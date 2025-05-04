d4fc93526e0e8565595837e9f2f11c4c4c148719
manweile <manweile@ualberta.ca>
2025-05-04 11:12:24 -0600

    doc and code changes

--- src/audio_info/audio_metadata.py
+++ src/audio_info/audio_metadata.py
@@ -2,24 +2,11 @@
 @file audio_metadata.py
 @brief Defines the audio metadata class
 
-@section description_audio_metadata Description
-Defines the base class fort audio metadata processing
-- AudioMetadata (base class)
-
-@section libraries_audio_metadata Libraries/Modules
-- eyed3 link
-    - IPSUM LOREM
-- mutagen link
-    - IPSUM LOREM
-
-@section author_audio_metadata Authour(s)
-- Created by Gerald Manweiler on April 8, 2025
-- Modified by Gerald Manweiler on April 22, 2025
-
-Copyright (c) 2025 GWN Software. All rights reserved
+@author Gerald Manweiler
+@copyright @showdate "%Y" GWN Software. All rights reserved.
 '''
 
-import eyed3
+# third party modules
 import mutagen
 
 
@@ -38,24 +25,24 @@ D:\\ProcessedMusic\\
 
 class AudioMetadata():
     '''
-    @brief Contains metadata processing functionality
-    @details Defines the base metadata processing used by project
+    @brief Defines the base metadata processing used by project.
     '''
 
     def __init__(self):
         '''
-        @brief Initializes the AudioMetadata class
-        @details A basic class implementation with no instantiation parameters
+        @brief Initializes the AudioMetadata class.
 
-        @return AudioMetadata {object} An instance of the class
+        @details A basic class implementation with no instantiation parameters.
+
+        @return AudioMetadata {instance} An instance of the class
         '''
 
         pass
 
 
-    # @todo finish, using mutagen
-    def get_album_name(self, audio_file):
+    def get_album_name(self, file_path):
         '''
+        @todo finish using mutagen
         @brief Gets album name from metadata.
 
         @param audio_file {object} The FileType instance for an audio file.
@@ -69,15 +56,14 @@ class AudioMetadata():
 
         # the album name we must get from metadata
 
-
         return artist_name, album_name
 
 
     def get_art_type(self, audio_file):
         '''
-        @brief returns the mime type of album art
+        @brief returns the mime type of album art.
 
-        @param audio_file {object} The FileType instance for an audio file
+        @param audio_file {object} The FileType instance for an audio file.
         @return mime_type {str} The mime type of the album art
         '''
 
@@ -88,78 +74,101 @@ class AudioMetadata():
 
         return mime_type
 
-
-    # @todo finish, using mutagen
-    def get_metadata_type(self, audio_file):
+    def get_file_type(self, file_path):
         '''
-        @brief Returns the metadata type of audio file
+        @todo finish using mutagen
+        @brief Returns the file type of audio file.
 
-        @param audio_file {object} The mutagen FileType instance for an audio file
-        @return metadata_type {str} The metadata type of the audio file
+        @details Returns the file type using audio metadata as opposed to getting it from os library.
+
+        @param file_path {str} The full path to audio file
+        @return file_type {str} The file type of audio file.
         '''
 
         pass
 
 
-    def get_tag_info(self, audio_file):
+    def get_metadata_type(self, file_path):
         '''
-        @brief gets tag information for an audio file
+        @brief Returns the metadata type of audio file.
 
-        @param audio_file  {object} The eyed3 Mp3AudioFile object for an audio file
+        @param file_path {str} The full path to audio file
+        @return metadata_type {str} The metadata type of the audio file
+        '''
+
+        metadata_type = None
+
+        try:
+            audio_file = mutagen.File(file_path)
+            if audio_file is not None:
+                metadata_type = audio_file.__class__.__name__
+            else:
+                return None
+        except Exception as e:
+            print(f"Error processing file: {e}")
+            return None
+
+        return metadata_type
+
+
+    def get_mp3_tag_info(self, file_path):
+        '''
+        @todo finish using mutagen
+        @brief gets tag information for an audio file.
+
+        @param file_path {str} The full path to audio file
         @return tag_info {object} Tag object holding audio file tag info
         '''
 
         tag_info = None
-        if audio_file != None:
-            tag_info = audio_file.tag
-
+        audio_file = self.load_mp3_file(file_path)
+        tag_info = audio_file.tags
         return tag_info
 
-    # @todo input a mutagen FileType instance instead of a file path
-    def has_art(self, file_path):
+    def mp3_has_art(self, file_path):
         '''
-        @brief Checks if an audio file contains embedded album art
+        @brief Checks if an mp3 audio file contains embedded album art.
 
         @param file_path {str} The full path to audio file
         @return art_present {boolean} Returns true if art is present, false otherwise
         '''
 
         art_present = False
+        audio_file = self.load_mp3_file(file_path)
 
-        audio_file = mutagen.File(file_path)
         if 'APIC:' in audio_file:
             art_present = True
             apic_frame = audio_file['APIC:']
-            print(f"Image MIME type: {apic_frame.mime}")
+            print(f"Image MIME type: {apic_frame.mime} in: {file_path}")
 
         return art_present
 
-    def load_file(self, file_path):
+    def load_mp3_file(self, file_path):
         '''
-        @brief loads an audio file
+        @brief Loads an mp3 audio file.
+
+        @details Expects a valid filepath to a mp3 type audio file.
 
         @param file_path {str} The full file path for audio file
-        @return Tuple [{Mp3AudioFile}, {FileType}] Containing objects for the input audio file path
+        @return audio_file {FileType} Containing objects for the input audio file path
         '''
 
-        eyed3_audio_file = None
-        mutagen_audio_file = None
+        audio_file = None
+        if file_path.endswith('.mp3'):
+            audio_file = mutagen.File(file_path)
 
-        eyed3_audio_file = eyed3.load(file_path)
-        mutagen_audio_file = mutagen.File(file_path)
+        return audio_file
 
-        # @todo add IOError handling for file path  is not a file
-
-        # @todo check for load returning a None - this indicates the file type (ie mime-type) is NOT an an mp3 file
-
-        return eyed3_audio_file, mutagen_audio_file
-
-    def show_metadata(self, audio_file):
+    def show_mp3_metadata(self, file_path):
         '''
-        @brief Shows mutagen info
+        @brief Shows mp3 audio file metadata.
 
-        @param audio_file {FileType} The mutagen FileType instance for an audio file
+        @details All mp3 files are presumed to be ID3v2.3 tags so we have consistent metadata fields.
+
+        @param file_path {str} The full file path for audio file
         '''
+
+        audio_file = self.load_mp3_file(file_path)
 
         artist_name = audio_file['TPE1'].text[0]
         album_name = audio_file['TALB'].text[0]
@@ -172,27 +181,13 @@ class AudioMetadata():
         print(song_genre)
 
 
-    def show_date(self, tag_info):
+    def show_mp3_date(self, tag_info):
         '''
-        @brief Show eyed3 tag date info
+        @todo write using mutagen presuming ID3v2.3 metadata
+        @brief Show metadata date info
 
         @param tag_info {object} Tag object holding audio file tag info
         '''
 
-        # @todo getBestDate() perhaps not best method
-        # look at other ways, at min, format print - all I really want is the YYYY
-        song_date = tag_info.getBestDate()
-
-        print(song_date.year)
-
-        # @todo possible write an album art extraction function
-        # use mutagen if I do
-        # this extracts album art, I just want to check if there is album art
-        # also, artist name and song title metadata must have legal file name chars
-        # eg. for $ Non Blondes, the song title What's Up? is illegal and will throw an error while trying to write image file.
-        # for image in tag_info.images:
-        #     image_file = open("{0}-{1}.jpg".format(artist_name, song_title), "wb")
-        #     print("Writing image file: {0}-{1}.jpg".format(artist_name, song_title))
-        #     image_file.write(image.image_data)
-        #     image_file.close()
+        pass
 
