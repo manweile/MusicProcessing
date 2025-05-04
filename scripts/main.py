3e1bfb07b48f6eefc4edc37fe8d9ddaf7d42de7a
manweile <manweile@ualberta.ca>
2025-05-04 13:45:38 -0600

    not needed

--- scripts/main.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-from src.dir_processing.directory_processing import DirectoryProcessing
-
-def main():
-    processing = DirectoryProcessing('H', 'Music')
-    processing.get_ext_list_files("aac")
-
-if __name__ == "__main__":
-    main()
