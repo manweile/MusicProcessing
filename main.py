b48ccd4c4ac29cd2152699849889ab36b4e4f87b
manweile <manweile@ualberta.ca>
2025-05-04 11:12:40 -0600

    testing changes

--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 '''
 
 from src.dir_processing.directory_processing import DirectoryProcessing
+from src.audio_info.audio_metadata import AudioMetadata
 
 def main():
     '''
@@ -14,8 +15,20 @@ def main():
     @details Takes command line arguments and executes per arguments.
     '''
 
-    processing = DirectoryProcessing('H', 'Music')
-    processing.ext_list_files("aac")
+    # processing = DirectoryProcessing('H', 'Music')
+    # processing.ext_list_files("aac")
+    metadata = AudioMetadata()
+    file_list = ["H:\\Music\\Kenny Rogers\\Daytime Friends - The Very Best of Kenny\\18 Lady.mp3",
+                 "H:\\Music\\Albert Collins\\Albert Collins - Trash Talkin'.mp3",
+                 "H:\\Music\\4 Non Blondes\\Bigger, Better, Faster, More!\\4 Non Blondes-What's Up.mp3",
+                ]
+
+    for song in file_list:
+        metadata_type = metadata.get_metadata_type(song)
+        print("Song: {0} has metadata type: {1}".format(song, metadata_type))
+
+        tag = metadata.get_mp3_tag_info(song)
+        print(tag)
 
 if __name__ == "__main__":
     '''
