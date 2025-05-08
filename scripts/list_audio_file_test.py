1f719a8ea634cd912999f94e56571c2edeef794e
manweile <manweile@ualberta.ca>
2025-05-08 16:55:05 -0600

    usign raw string literal

--- scripts/list_audio_file_test.py
+++ scripts/list_audio_file_test.py
@@ -1,5 +1,5 @@
 from src.dir_processing import DirectoryProcessing
 
-dir_processing = DirectoryProcessing('H', 'Music')
+dir_processing = DirectoryProcessing(r"H:\Music")
 
 dir_processing.get_audio_list_files()
