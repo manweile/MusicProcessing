e2208b65bb048c4246d33ec569ff36cd2306b5be
manweile <manweile@ualberta.ca>
2025-05-08 17:52:59 -0600

    fix module not found

--- scripts/list_audio_file_test.py
+++ scripts/list_audio_file_test.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from src.dir_processing import DirectoryProcessing
 
 dir_processing = DirectoryProcessing(r"H:\Music")
