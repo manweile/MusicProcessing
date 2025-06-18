dbddbae8ff097b78b7124430c51e63e61f9df586
manweile <manweile@ualberta.ca>
2025-06-18 10:56:45 -0600

    single quote to double quote

--- src/__init__.py
+++ src/__init__.py
@@ -8,12 +8,12 @@
 ## @var _AUDIO_EXTS
 # @brief audio file extensions in my collection
 # @details use this when working with os library
-_AUDIO_EXTS = [".mp3", ".m4a", ".wma", ".wav"]
+_AUDIO_EXTS = [".mp3", ".m4a", ".wma"]
 
 ## @var _AUDIO_TYPES
 # @brief audio file types in my collection
 # @details use this when you just need the type
-_AUDIO_TYPES = ["mp3", "m4a", "wma", "wav"]
+_AUDIO_TYPES = ["mp3", "m4a", "wma"]
 
 ## @var _PLAYLIST_EXTS
 # @brief playlist file extensions in my collection
@@ -31,4 +31,4 @@ _PLAYLIST_TYPES = ["m3u"]
 # @details  In modules needing the constant add `from src._AUDIO_TYPES import _AUDIO_TYPES`
 # @details  In modules needing the constant add `from src._PLAYLIST_EXTS import _PLAYLIST_EXTS`
 # @details  In modules needing the constant add `from src._PLAYLIST_TYPES import _PLAYLIST_TYPES`
-__all__ = ['_AUDIO_EXTS','_AUDIO_TYPES', '_PLAYLIST_EXTS', '_PLAYLIST_TYPES']
+__all__ = ["_AUDIO_EXTS", "_AUDIO_TYPES", "_PLAYLIST_EXTS", "_PLAYLIST_TYPES"]
