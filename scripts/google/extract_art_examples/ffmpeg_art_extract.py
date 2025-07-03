54e66d180ffb1cb4afa9472e5ec38728d0f33798
manweile <manweile@ualberta.ca>
2025-07-03 16:51:55 -0600

    add new cli

--- scripts/google/extract_art_examples/ffmpeg_art_extract.py
+++ scripts/google/extract_art_examples/ffmpeg_art_extract.py
@@ -1,5 +1,6 @@
 import platform
 import subprocess
+from subprocess import CalledProcessError
 
 
 def extract_album_art(input_audio_file, output_image_file):
@@ -10,28 +11,45 @@ def extract_album_art(input_audio_file, output_image_file):
         input_audio_file (str): Path to the input audio file (e.g., 'song.mp3').
         output_image_file (str): Path to save the extracted album art (e.g., 'cover.jpg').
     """
+    # this results in 28 kB file cor Crush-Live.mp3
     command = [
         'ffmpeg', '-hide_banner',
         '-i', input_audio_file,
         '-an',
-        '-c:v', 'copy',
+        '-vcodec', 'copy',
+        "-update", "1",
         output_image_file, '-y'
     ]
+    '''
+    this results in 11.5 kb file for Crush-Live.mp3
+    command = [
+        'ffmpeg', '-hide_banner',
+        '-i', input_audio_file,
+        '-an',
+        '-map', '0:v',
+        '-map_metadata', '-1'
+        '-update', '1',
+        output_image_file, '-y'
+    ]
+
+    '''
     try:
-        subprocess.run(command, capture_output=True, text=True)
+        subprocess.run(command, check=True, capture_output=True, text=True)
+
         print(f"Album art extracted successfully to {output_image_file}")
-    except subprocess.CalledProcessError as e:
+    except CalledProcessError as e:
         print(f"Error extracting album art: {e}")
         print(f"FFmpeg output: {e.stderr}")
 
 
-file_path = None
 if platform.system() == "Linux":
     file_path = r"/home/gerald/Music/Crush/Here/Crush-Live.mp3"  # has art
+    extracted_art = r"/home/gerald/MusicProcessing/src/generated_files/Crush/Here/ffmpeg-Extracted.jpg"
 elif platform.system() == "Windows":
     file_path = r"C:\Music\Crush\Here\Crush-Live.mp3"  # has art
+    extracted_art = r"D:\MusicProcessing\src\generated_files\Music\Crush\Here\ffmpeg-Extracted.jpg"
 
-extracted_art = r"/home/gerald/MusicProcessing/src/generated_files/Crush/Here/Extracted.jpg"
+
 
 # Example usage:
 extract_album_art(file_path, extracted_art)
