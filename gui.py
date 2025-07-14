950d65024dd5b9d7b7046f500afbd0f4b58c5322
manweile <manweile@ualberta.ca>
2025-07-14 16:25:30 -0600

    added extract file and walk

--- gui.py
+++ gui.py
@@ -38,6 +38,27 @@ def convert_file(file_path):
     metadata.convert_file(file_path)
 
 
+def extract_file(file_path):
+    '''
+    @brief Extracts and saves embedded album art from specified audio file.
+
+    @param file_path {str} The full path to audio file.
+    '''
+
+    art.extract_album_art(file_path)
+
+
+def extract_walk(tld_path, file_pattern):
+    '''
+    @brief Extracts and save embedded art from  all audio files in specified top level directory with specified pattern.
+
+    @param tld_path {str} The top level directory path that contains all the music files.
+    @param file_pattern {str} The file pattern we want to delete.
+    '''
+
+    art.extract_walk(tld_path, file_pattern)
+
+
 @Gooey(optional_cols=2, program_name="Music Processing")
 def parse_args():
     '''
@@ -50,10 +71,31 @@ def parse_args():
     parser = GooeyParser(description='Music Processing')
     subparsers = parser.add_subparsers(title="subcommands", dest="subcommand")
 
+    # convert audio file specified to mp3 format
+    # 1 mandatory arg, the audio file path
+    # sys.argv = ['D:\MusicProcessing\main.py', 'convert-file', 'C:\Music\Joshua Davis\The Voice Peformance\Joshua Davis-The Workingman's Hymn.m4a']
+    # sys.argv = ['/home/gerald/MusicProcessing/main.py', 'convert-file', '/home/gerald/Music/Joshua Davis/The Voice Peformance/Joshua Davis-The Workingman's Hymn.m4a']
     convert_file_parser = subparsers.add_parser("convert-file", help="Converts an audio file to mp3")
     convert_file_parser.add_argument("file", type=str, help="mandatory full path to audio file", widget="FileChooser")
     convert_file_parser.set_defaults(func=convert_file)
 
+    # extract album art from specified audio file
+    # 1 mandatory arg, the path to audio file
+    # sys.argv = ['D:\MusicProcessing\main.py', 'extract-art', 'C:\Music\Elton John\Goodbye Yellow Brick Road\Elton John-Saturday Night's Alright for Fighting.wma',
+    # sys.argv = ['/home/gerald/MusicProcessing/main.py', 'extract-art', '/home/gerald/Music/Elton John/Goodbye Yellow Brick Road/Elton John-Saturday Night's Alright for Fighting.wma',
+    extract_file_parser = subparsers.add_parser("extract-file", help="Extracts embedded art from audio file")
+    extract_file_parser.add_argument("file", type=str, help="mandatory full path to audio file", widget="FileChooser")
+    extract_file_parser.set_defaults(func=extract_file)
+
+    # extract album art from all audio files found in top level directory
+    # 1 mandatory arg, the tld path
+    # 1 optional arg, the file pattern to match
+    # sys.argv = ['D:\MusicProcessing\main.py', 'extract-walk', 'C:\Music', '--pattern', { '*.mp3' | '*.m4a' | '*.wma' } ]
+    # sys.argv = ['/home/gerald/MusicProcessing/main.py', 'extract-walk', '/home/gerald/Music', '--pattern', { '*.mp3' | '*.m4a' | '*.wma' } ]
+    extract_walk_parser = subparsers.add_parser("extract-walk", help="Extracts embedded art from all audio files")
+    extract_walk_parser.add_argument("tld", type=str, help="mandatory top level directory", widget="DirChooser")
+    extract_walk_parser.add_argument("--pattern", type=str, help="optional file pattern")
+    extract_walk_parser.set_defaults(func=extract_walk)
     args = parser.parse_args()
     return args
 
@@ -73,6 +115,15 @@ def main(args):
         if args.subcommand == "convert-file":
             file_path = getattr(args, "file")
             convert_file(file_path)
+
+        if args.subcommand == "extract-file":
+            file_path = getattr(args, "file")
+            extract_file(file_path)
+
+        if args.subcommand == "extract-walk":
+            tld_path = getattr(args, "tld")
+            file_pattern = getattr(args, "pattern")
+            extract_walk(tld_path, file_pattern)
 
     except NotImplementedError:
         raise NotImplementedError(f"Command {args.subcommand} does not exist")
