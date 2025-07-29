3247a510bab55ceb2f6f4e16cf1fa93f9a7a4709
manweile <manweile@ualberta.ca>
2025-07-29 15:07:30 -0600

    update templating with logging configuration

--- scripts/google/templating/my_module.py
+++ scripts/google/templating/my_module.py
@@ -9,14 +9,27 @@
 # standard modules
 import gc
 import logging
+import os
 
 # third party modules
 # import ipsumlorem
 
 # local modules
-# import ipsumlorem
+from src import FILE_LOG_FORMAT, LOG_DIR, LOG_EXT, UTF8          # logging modules
+from src.generated_files import GENERATED_FILES
 
 gc.enable()
+
+# Configure logging
+basename = os.path.basename(__file__)
+stem = os.path.splitext(basename)[0]
+file = stem + LOG_EXT
+log_filename = os.path.join(GENERATED_FILES, LOG_DIR, file)
+
+logging.basicConfig(filename=log_filename, level=logging.DEBUG, format=FILE_LOG_FORMAT, filemode="a", encoding=UTF8)
+logger = logging.getLogger(__name__)
+# override the default logging level WARN to lowest level so we can log all level messages
+logger.setLevel(logging.DEBUG)
 
 
 class MyClass():
