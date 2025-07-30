a9be400dc71ce80c5a6d4dcc9469d1c48164153d
manweile <manweile@ualberta.ca>
2025-07-29 22:20:27 -0600

    re-ordering

--- src/audio_info/audio_playlist.py
+++ src/audio_info/audio_playlist.py
@@ -14,27 +14,29 @@ from pathlib import Path
 
 # local modules
 from src import AUDIO_EXTS
-from src import FILE_LOG_FORMAT, LOG_DIR, LOG_EXT, UTF8          # logging modules
+from src import ERROR_LOG_FORMAT, LOG_DIR, LOG_EXT, UTF8          # logging constants
 from src import PLAYLIST_EXTS
-from src.dir_processing import DirectoryProcessing
 from src.generated_files import GENERATED_FILES
+from src.dir_processing import DirectoryProcessing
 
 gc.enable()
+
+directory = DirectoryProcessing()
 
 # Configure logging
 basename = os.path.basename(__file__)
 stem = os.path.splitext(basename)[0]
 file = stem + LOG_EXT
 log_filename = os.path.join(GENERATED_FILES, LOG_DIR, file)
+# override the default logging level WARN to lowest level so we can log all levels
+logging.basicConfig(filename=log_filename, level=logging.DEBUG, format=ERROR_LOG_FORMAT, filemode="a", encoding=UTF8)
 
-logging.basicConfig(filename=log_filename, level=logging.DEBUG, format=FILE_LOG_FORMAT, filemode="a", encoding=UTF8)
+# create logger for module and restrict to module
+# use raise in exception handling if we need send something inter-module
 logger = logging.getLogger(__name__)
-# override the default logging level WARN to lowest level so we can log all level messages
-logger.setLevel(logging.DEBUG)
+logger.propagate = False
 
 _DELIMITER = ","
-
-directory = DirectoryProcessing()
 
 
 class AudioPlaylist():
--- src/audio_normalize/audio_normalization.py
+++ src/audio_normalize/audio_normalization.py
@@ -24,22 +24,26 @@ from yaspin.spinners import Spinners
 
 # local modules
 from src import AUDIO_EXTS, AUDIO_TYPES
-from src import FILE_LOG_FORMAT, LOG_DIR, LOG_EXT, UTF8          # logging modules
-from src.dir_processing import DirectoryProcessing
+from src import ERROR_LOG_FORMAT, LOG_DIR, LOG_EXT, UTF8          # logging constants
 from src.generated_files import GENERATED_FILES
+from src.dir_processing import DirectoryProcessing
 
 gc.enable()
+
+directory = DirectoryProcessing()
 
 # Configure logging
 basename = os.path.basename(__file__)
 stem = os.path.splitext(basename)[0]
 file = stem + LOG_EXT
 log_filename = os.path.join(GENERATED_FILES, LOG_DIR, file)
+# override the default logging level WARN to lowest level so we can log all levels
+logging.basicConfig(filename=log_filename, level=logging.DEBUG, format=ERROR_LOG_FORMAT, filemode="a", encoding=UTF8)
 
-logging.basicConfig(filename=log_filename, level=logging.DEBUG, format=FILE_LOG_FORMAT, filemode="a", encoding=UTF8)
+# create logger for module and restrict to module
+# use raise in exception handling if we need send something inter-module
 logger = logging.getLogger(__name__)
-# override the default logging level WARN to lowest level so we can log all level messages
-logger.setLevel(logging.DEBUG)
+logger.propagate = False
 
 r'''
 AES https://www.aes.org/technical/documents/AESTD1004_1_15_10.pdf
@@ -53,8 +57,6 @@ I will keep that cause I want the extra headroom space vs -1.0 or 0.0
 ILT = "-16.0"
 LRA = "11.0"
 TP = "-2.0"
-
-directory = DirectoryProcessing()
 
 
 class AudioNormalization():
