c04ac6b1aec0fe271c63bf9a73095321b9a7516e
manweile <manweile@ualberta.ca>
2025-07-29 22:19:54 -0600

    tweaked log levels and added custom error

--- src/audio_info/audio_metadata.py
+++ src/audio_info/audio_metadata.py
@@ -28,28 +28,33 @@ from tqdm import tqdm
 # local modules
 from src import AUDIO_EXTS, AUDIO_FILES, AUDIO_TYPES
 from src import FOLDER_ART
-from src import FILE_LOG_FORMAT, LOG_DIR, LOG_EXT, UTF8          # logging modules
+from src import ERROR_LOG_FORMAT, LOG_DIR, LOG_EXT, UTF8          # logging constants
+from src.generated_files import GENERATED_FILES
+from src import PathInfoError
 from src.audio_normalize import AudioNormalization
 from src.dir_processing import DirectoryProcessing
-from src.generated_files import GENERATED_FILES
 
 gc.enable()
+
+directory = DirectoryProcessing()
+normalization = AudioNormalization()
 
 # Configure logging
 basename = os.path.basename(__file__)
 stem = os.path.splitext(basename)[0]
 file = stem + LOG_EXT
 log_filename = os.path.join(GENERATED_FILES, LOG_DIR, file)
+# override the default logging level WARN to lowest level so we can log all levels
+logging.basicConfig(filename=log_filename, level=logging.DEBUG, format=ERROR_LOG_FORMAT, filemode="a", encoding=UTF8)
 
-logging.basicConfig(filename=log_filename, level=logging.DEBUG, format=FILE_LOG_FORMAT, filemode="a", encoding=UTF8)
+# create logger for module and restrict to module
+# use raise in exception handling if we need send something inter-module
 logger = logging.getLogger(__name__)
-# override the default logging level WARN to lowest level so we can log all level messages
-logger.setLevel(logging.DEBUG)
+logger.propagate = False
 
 # metadata class needs 2 file handlers:
 # information for normal audio processing events
 # error for abnormal situations
-
 
 TPOS = "TPOS"
 TYER = "TYER"
@@ -148,9 +153,6 @@ WMA_TIME_KEYS = {
     'WM/OriginalReleaseYear'
 }
 
-directory = DirectoryProcessing()
-normalization = AudioNormalization()
-
 
 class AudioMetadata():
     '''
@@ -218,6 +220,10 @@ class AudioMetadata():
             # path_info does an ext check and logs if unacceptable ext & prints to console
             # for or raises Exception for unhandled errors
             export_path = directory.path_info(file_path)
+
+            if not export_path:
+                logger.error(f"No export path created for {file_path}")
+                raise PathInfoError
 
             # @todo check for a Folder.jpg colocated with audio file
 
