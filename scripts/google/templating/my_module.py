f00d63085bb91d64b0272c052366bf40b93c4fa2
manweile <manweile@ualberta.ca>
2025-07-29 22:18:09 -0600

    re-ordering and refactoring

--- scripts/google/templating/my_module.py
+++ scripts/google/templating/my_module.py
@@ -15,7 +15,7 @@ import os
 # import ipsumlorem
 
 # local modules
-from src import FILE_LOG_FORMAT, LOG_DIR, LOG_EXT, UTF8          # logging constants
+from src import ERROR_LOG_FORMAT, LOG_DIR, LOG_EXT, UTF8          # logging constants
 from src.generated_files import GENERATED_FILES
 
 gc.enable()
@@ -26,7 +26,7 @@ stem = os.path.splitext(basename)[0]
 file = stem + LOG_EXT
 log_filename = os.path.join(GENERATED_FILES, LOG_DIR, file)
 # override the default logging level WARN to lowest level so we can log all levels
-logging.basicConfig(filename=log_filename, level=logging.DEBUG, format=FILE_LOG_FORMAT, filemode="a", encoding=UTF8)
+logging.basicConfig(filename=log_filename, level=logging.DEBUG, format=ERROR_LOG_FORMAT, filemode="a", encoding=UTF8)
 
 # create logger for module and restrict to module
 # use raise in exception handling if we need send something inter-module
