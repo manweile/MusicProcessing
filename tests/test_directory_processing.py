485ceb68d5222a73440f0efd5d1050b747d27d12
manweile <manweile@ualberta.ca>
2025-08-07 14:42:17 -0600

    remove logging & error handling, add mocking

--- tests/test_directory_processing.py
+++ tests/test_directory_processing.py
@@ -9,33 +9,16 @@
 
 # standard modules
 import gc
-import logging
 import os
 import unittest
-
-# third party modules
-# import ipsumlorem
+from unittest.mock import patch
 
 # local modules
+from src import AUDIO_TYPES
 from src import EXPORT_TLD
-from src import ERROR_LOG_FORMAT, LOG_DIR, LOG_EXT, UTF8          # logging constants
-from src.generated_files import GENERATED_FILES
 from src.dir_processing import DirectoryProcessing
 
 gc.enable()
-
-# Configure logging
-basename = os.path.basename(__file__)
-stem = os.path.splitext(basename)[0]
-file = stem + LOG_EXT
-log_filename = os.path.join(GENERATED_FILES, LOG_DIR, file)
-# override the default logging level WARN to lowest level so we can log all levels
-logging.basicConfig(filename=log_filename, level=logging.DEBUG, format=ERROR_LOG_FORMAT, filemode="a", encoding=UTF8)
-
-# create logger for module and restrict to module
-# use raise in exception handling if we need send something inter-module
-logger = logging.getLogger(__name__)
-logger.propagate = False
 
 # instantiate module levels vars here
 TESTS_PATH = os.path.dirname(os.path.abspath(__file__))
@@ -49,14 +32,16 @@ class TestDirectoryProcessing(unittest.TestCase):
     @brief Tests DirectoryProcessing class functions.
     '''
 
-    def test_path_info_not_audio(self):
+    @patch('src.dir_processing.directory_processing.logger.warning')
+    def test_path_info_not_audio(self, mock_warning):
         '''
         @brief Tests if trying to get path info for a non-audio file.
         '''
 
-        m3u_file = os.path.join(TESTS_PATH, EXPORT_TLD, "expected.m3u")
-        path_info = directory.path_info(m3u_file)
+        input_path = os.path.join(TESTS_PATH, EXPORT_TLD, "expected.m3u")
+        path_info = directory.path_info(input_path)
         self.assertIsNone(path_info)
+        mock_warning.assert_called_once_with(f"File {input_path} is not in {AUDIO_TYPES}")
 
 
 if __name__ == "__main__":
