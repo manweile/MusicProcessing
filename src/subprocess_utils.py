0e337b7ba6fb42c97ba0df66719693b390a71314
manweile <manweile@ualberta.ca>
2025-08-07 10:41:40 -0600

    add the subprocess functions fro audio normalization

--- src/subprocess_utils.py
+++ src/subprocess_utils.py
@@ -10,9 +10,12 @@
 import gc
 import logging
 import os
+import subprocess
+from subprocess import CalledProcessError
 
 # third party modules
-# import ipsumlorem
+from yaspin import yaspin
+from yaspin.spinners import Spinners
 
 # local modules
 from src import ERROR_LOG_FORMAT, LOG_DIR, LOG_EXT, UTF8          # logging constants
@@ -49,3 +52,72 @@ class SubprocessUtilities():
         '''
 
         pass
+
+
+    def spinner_subprocess_run(self, text, command):
+        '''
+        @brief Runs command in subprocess with a spinner.
+
+        @details Runs subprocess for command, returns stdin & stderr.
+
+        @param text {str} Text for spinner to display.
+        @param command {str} Command for subprocess  to run.
+        @return results (process, spinner) ({CompletedProcess}, {Yaspin}) Tuple containing completed process and spinner objects.
+        @exception CalledProcessError A subprocess error from ffmpeg command execution.
+        @exception Exception A common baseclass exception to handle unforeseen errors.
+        '''
+
+        try:
+            with yaspin(Spinners.dots, text=text, timer=True) as spinner:
+                # check enables CalledProcessError throwing,
+                # capture output to get stdout & stderr
+                # text decodes stdout/stderr as text
+                process = subprocess.run(
+                    command,
+                    check=True,
+                    capture_output=True,
+                    text=True
+                )
+
+        except CalledProcessError as e:
+            logger.exception(f"CalledProcessError returncode:{e.returncode}, with stderr: {e.stderr} on command {e.cmd}", stack_info=True)
+            raise
+        except Exception:
+            logger.exception(f"Exception processing command: {command}", stack_info=True)
+            raise
+        else:
+            return process, spinner
+
+
+    def subprocess_run(self, command):
+        '''
+        @brief Runs command in subprocess.
+
+        @details Runs subprocess for command, returns stdin & stderr.
+
+        @param text {str} Text for spinner to display.
+        @param command {str} Command for subprocess  to run.
+        @return process {CompletedProcess} Completed process object.
+        @exception CalledProcessError A subprocess error from ffmpeg command execution.
+        @exception Exception A common baseclass exception to handle unforeseen errors.
+        '''
+
+        try:
+            # check enables CalledProcessError throwing,
+            # capture output to get stdout & stderr
+            # text decodes stdout/stderr as text
+            process = subprocess.run(
+                command,
+                check=True,
+                capture_output=True,
+                text=True
+            )
+
+        except CalledProcessError as e:
+            logger.exception(f"CalledProcessError returncode:{e.returncode}, with stderr: {e.stderr} on command {e.cmd}", stack_info=True)
+            raise
+        except Exception:
+            logger.exception(f"Exception processing command: {command}", stack_info=True)
+            raise
+        else:
+            return process
