f6ec49a1f118efef30e5988ae5f67de0f035f9a9
manweile <manweile@ualberta.ca>
2025-08-09 21:12:53 -0600

    removed logging

--- tests/test_audio_playlist.py
+++ tests/test_audio_playlist.py
@@ -5,32 +5,18 @@
 @author Gerald Manweiler
 @copyright @showdate "%Y" GWN Software. All rights reserved.
 '''
+
 # standard modules
 import gc
-import logging
 import os
 import unittest
 
 # local modules
 from src import EXPORT_TLD
-from src import ERROR_LOG_FORMAT, LOG_DIR, LOG_EXT, UTF8          # logging constants
 from src.generated_files import GENERATED_FILES
 from src.audio_info import AudioPlaylist
 
 gc.enable()
-
-# Configure logging
-basename = os.path.basename(__file__)
-stem = os.path.splitext(basename)[0]
-file = stem + LOG_EXT
-log_filename = os.path.join(GENERATED_FILES, LOG_DIR, file)
-# override the default logging level WARN to lowest level so we can log all levels
-logging.basicConfig(filename=log_filename, level=logging.DEBUG, format=ERROR_LOG_FORMAT, filemode="a", encoding=UTF8)
-
-# create logger for module and restrict to module
-# use raise in exception handling if we need send something inter-module
-logger = logging.getLogger(__name__)
-logger.propagate = False
 
 TESTS_PATH = os.path.dirname(os.path.abspath(__file__))
 TESTS_TLD = os.path.join(TESTS_PATH, EXPORT_TLD)
@@ -51,12 +37,10 @@ class TestAudioPlaylist(unittest.TestCase):
         '''
 
         input_m3u = os.path.join(TESTS_TLD, "test.m3u")
-        input_tld = TESTS_TLD
-        input_tld = TESTS_TLD
         generated_m3u = os.path.join(GENERATED_FILES, "test.m3u")
         expected_m3u = os.path.join(TESTS_TLD, "expected.m3u")
 
-        playlist.update_paths(input_tld, input_m3u)
+        playlist.update_paths(TESTS_TLD, input_m3u)
 
         self.assertTrue(os.path.exists(generated_m3u))
 
