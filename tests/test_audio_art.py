124099634b92dbfcca09949bd53252f8578da64b
manweile <manweile@ualberta.ca>
2025-08-28 12:23:22 -0600

    remove decorators

--- tests/test_audio_art.py
+++ tests/test_audio_art.py
@@ -14,7 +14,7 @@ import os
 import unittest
 from pathlib import Path
 from subprocess import CalledProcessError
-from unittest.mock import patch
+from unittest import TestCase
 
 # local modules
 from src import EXPORT_TLD
@@ -27,14 +27,16 @@ gc.enable()
 TESTS_PATH = os.path.dirname(os.path.abspath(__file__))
 TESTS_TLD = os.path.join(TESTS_PATH, EXPORT_TLD)
 
+# don't add this one to delete list, need it for an error test
+EXPECTED_FOUND_ALBUM_ART_JPG = os.path.join(TESTS_TLD, "Abba", "Waterloo", FOLDER_ART)
+
 EXPECTED_M4A_JPG = os.path.join(TESTS_TLD, "Joshua Davis", "The Voice Peformance", FOLDER_ART)
 EXPECTED_MP3_JPG = os.path.join(TESTS_TLD, "Crush", "Here", FOLDER_ART)
 EXPECTED_NO_STREAM_JPG = os.path.join(TESTS_TLD, "Billie Holiday", "Georgia On My Mind", FOLDER_ART)
-EXPECTED_NO_TAG_MP3_JPG = os.path.join(TESTS_TLD, FOLDER_ART)
 EXPECTED_SET_ALBUM_ART_JPG = os.path.join(TESTS_TLD, "Albert Collins", "Best Of The Blues, Vol. 1", FOLDER_ART)
 EXPECTED_WMA_JPG = os.path.join(TESTS_TLD, "Elton John", "Goodbye Yellow Brick Road", FOLDER_ART)
 
-EXPECTED_JPGS = [EXPECTED_MP3_JPG, EXPECTED_M4A_JPG, EXPECTED_NO_STREAM_JPG, EXPECTED_WMA_JPG, EXPECTED_SET_ALBUM_ART_JPG]
+DELETE_JPGS = [EXPECTED_M4A_JPG, EXPECTED_MP3_JPG, EXPECTED_NO_STREAM_JPG, EXPECTED_SET_ALBUM_ART_JPG, EXPECTED_WMA_JPG]
 
 INPUT_M3U = os.path.join(TESTS_TLD, "test.m3u")
 
@@ -56,7 +58,7 @@ encapsulate the assertRaises in a try block, and use a finally to restore the or
 original_log_level = logging.getLogger().getEffectiveLevel()
 
 
-class TestAudioArt(unittest.TestCase):
+class TestAudioArt(TestCase):
     '''
     @brief Tests AudioArt class functions.
     '''
@@ -66,45 +68,44 @@ class TestAudioArt(unittest.TestCase):
         @brief Clean up the created Folder.jpg files.
         '''
 
-        for jpg in EXPECTED_JPGS:
+        for jpg in DELETE_JPGS:
             if os.path.exists(jpg):
                 os.remove(jpg)
 
 
-    @patch('src.audio_info.audio_art.logger.info')
-    def test_extract_album_art_folder_exists(self, mock_warning):
+    def test_extract_album_art_folder_exists(self):
         '''
         brief Tests try to extract album art from file that already has co-located Folder.jpg file.
         '''
 
         input_audio = SRC_HAS_JPG_AUDIO
         album_path = Path(input_audio).parent
-        art.extract_album_art(input_audio)
-        mock_warning.assert_called_once_with(f"{album_path} has a {FOLDER_ART}")
+        log_msg = f"{album_path} has a {FOLDER_ART}"
+
+        with self.assertLogs() as captured:
+            art.extract_album_art(input_audio)
+
+        self.assertEqual(len(captured.records), 1)
+        self.assertEqual(captured.records[0].getMessage(), log_msg)
 
 
-    @patch('src.audio_info.audio_art.logger.info')
-    def test_extract_album_art_invalid_audio(self, mock_warning):
+    def test_extract_album_art_invalid_audio(self):
         '''
         @brief Tests try to extract album art from non-valid file.
         '''
 
         input_audio = INPUT_M3U
         input_path = Path(input_audio)
-        art.extract_album_art(input_audio)
-        mock_warning.assert_called_once_with(f"{input_path.name} is not an audio file")
+        log_msg = f"{input_path.name} is not an audio file"
+
+        with self.assertLogs() as captured:
+            art.extract_album_art(input_audio)
+
+        self.assertEqual(len(captured.records), 1)
+        self.assertEqual(captured.records[0].getMessage(), log_msg)
 
 
-    # per https://stackoverflow.com/questions/15763394/mocking-two-functions-with-patch-for-a-unit-test
-    # the order of stacked patch decorators and calls to the matching assert_called_once_with matter,
-    # and its FILO (first in, last out)
-    # so if a warning is 1st patch, then the same warning has to be last assert
-    # the declaration order in signature doesn't matter though
-
-
-    @patch('src.audio_info.audio_art.logger.warning')
-    @patch('src.audio_info.audio_art.logger.info')
-    def test_extract_album_art_no_tag_or_stream(self, mock_info, mock_warning):
+    def test_extract_album_art_no_tag_or_stream(self):
         '''
         @brief Test try to extract art from audio file with no stream or metadata tags at all.
 
@@ -113,11 +114,19 @@ class TestAudioArt(unittest.TestCase):
         '''
 
         input_audio = SRC_NO_TAG_MP3
-        art.extract_album_art(input_audio)
-        art_exists = os.path.exists(EXPECTED_NO_TAG_MP3_JPG)
+        no_jpg = os.path.join(TESTS_TLD, FOLDER_ART)
+        info_msg = f"No video stream album art present in {input_audio}"
+        warning_msg = f"No album art present in {input_audio}"
+
+        with self.assertLogs() as captured:
+            art.extract_album_art(input_audio)
+
+        art_exists = os.path.exists(no_jpg)
         self.assertFalse(art_exists)
-        mock_info.assert_called_once_with(f"No video stream album art present in {input_audio}")
-        mock_warning.assert_called_once_with(f"No album art present in {input_audio}")
+
+        self.assertEqual(len(captured.records), 2)
+        self.assertEqual(captured.records[0].getMessage(), info_msg)
+        self.assertEqual(captured.records[1].getMessage(), warning_msg)
 
 
     def test_extract_album_art_with_stream_and_tag(self):
@@ -177,18 +186,13 @@ class TestAudioArt(unittest.TestCase):
 
         input_audio = SRC_NO_STREAM_WMA
 
-        logging.disable(logging.ERROR)
+        with self.assertRaises(CalledProcessError) as cm:
+            art.extract_ffmpeg_art(input_audio)
 
-        try:
-            with self.assertRaises(CalledProcessError) as cm:
-                art.extract_ffmpeg_art(input_audio)
-
-            art_exists = os.path.exists(EXPECTED_NO_STREAM_JPG)
-            self.assertFalse(art_exists)
-            # Invalid argument is ffmpeg saying no video stream present
-            self.assertTrue("Invalid argument" in cm.exception.stderr.strip())
-        finally:
-            logging.disable(original_log_level)
+        art_exists = os.path.exists(EXPECTED_NO_STREAM_JPG)
+        self.assertFalse(art_exists)
+        # Invalid argument is ffmpeg saying no video stream present
+        self.assertTrue("Invalid argument" in cm.exception.stderr.strip())
 
 
     def test_extract_m4a_art(self):
@@ -238,11 +242,13 @@ class TestAudioArt(unittest.TestCase):
         @brief Tests setting album art file.
         '''
 
-        # @todo turning off logging
         art.set_album_art(TESTS_TLD)
-        # @todo assert exists abba/waterloo/folder.jpg
-        # @todo assert exists albert collins/best of the blues, vol. 1/folder.jpg
-        # @todo turning on logging
+
+        found_art_exists = os.path.exists(EXPECTED_FOUND_ALBUM_ART_JPG)
+        set_art_exists = os.path.exists(EXPECTED_SET_ALBUM_ART_JPG)
+
+        self.assertTrue(found_art_exists)
+        self.assertTrue(set_art_exists)
 
 
 def get_method_names(cls):
