8a477fb37d43a178483069235b413eec1678ddb1
manweile <manweile@ualberta.ca>
2025-08-30 22:08:51 -0600

    debugging & adding tests

--- tests/test_audio_normalization.py
+++ tests/test_audio_normalization.py
@@ -10,6 +10,7 @@
 import gc
 import inspect
 import logging
+import math
 import os
 import shutil
 import unittest
@@ -19,7 +20,7 @@ from unittest import TestCase
 from src import EXPORT_TLD
 from src.generated_files import GENERATED_FILES
 from tests import TEST_MP3_ABBA, TEST_MP3_CRUSH
-from tests import TESTS_PATH
+from tests import TESTS_TLD
 # local module classes
 from src.audio_normalize import AudioNormalization
 
@@ -27,23 +28,20 @@ gc.enable()
 
 NORM_PATH = os.path.join(GENERATED_FILES, EXPORT_TLD)
 
-EBU_DYNAMIC_SRC = SAMPLE_RATE_SRC = TEST_MP3_ABBA
-EBU_DYNAMIC_RES = os.path.join(NORM_PATH, "Abba", "Waterloo", "ABBA-Waterloo.mp3")
+BIT_SRC = VOL_SRC = TEST_MP3_CRUSH
+BIT_RES = 129156
 
-SAMPLE_RATE_RES = 44100
+EBU_DYNAMIC_SRC = RMS_SRC = SAMPLE_RATE_SRC = TEST_MP3_ABBA
+EBU_DYNAMIC_RES = RMS_RES = os.path.join(NORM_PATH, "Abba", "Waterloo", "ABBA-Waterloo.mp3")
 
 EBU_LINEAR_SRC = LOUDNORM_SRC = PEAK_SRC = RMS_CLIPPING_SRC = TEST_MP3_CRUSH
 EBU_LINEAR_RES = PEAK_RES = RMS_CLIPPING_RES = os.path.join(NORM_PATH, "Crush", "Here", "Crush-Live.mp3")
 
-SRC_FILE = TEST_MP3_CRUSH
+SAMPLE_RATE_RES = 44100
 
-# BIT_SRC = VOL_SRC = os.path.join(TESTS_PATH, EXPORT_TLD, "Bear McCreary", "Battlestar Galactica", "Bear McCreary - BSG Gayatri Mantra Theme Song.mp3")
-BIT_SRC = VOL_SRC = TEST_MP3_CRUSH
-# BIT_RES = 128969
-BIT_RES = 129156
+VOL_INFO_RES = {'mean_volume': -19.9, 'max_volume': -6.7}
 
-# VOLUME_INFO_RES = {'mean_volume': -15.8, 'max_volume': -0.1}
-VOLUME_INFO_RES = {'mean_volume': -19.9, 'max_volume': -6.7}
+VOL_SRC_FAIL = os.path.join(TESTS_TLD, "Bear McCreary", "Battlestar Galactica", "Bear McCreary - BSG Gayatri Mantra Theme Song.mp3")
 
 normalization = AudioNormalization()
 
@@ -60,16 +58,6 @@ class TestAudioNormalization(TestCase):
 
         if os.path.exists(NORM_PATH):
             shutil.rmtree(NORM_PATH)
-
-
-    # @todo add __loudnorn_json_parse tests
-    # can have JSONDecodeError
-    # can have JSONOutputError
-
-
-    # @todo add get_sample_rate tests
-    # can have IndexError
-    # can have JSONDecodeError
 
 
     def test_ebu_normalize_linear(self):
@@ -90,46 +78,22 @@ class TestAudioNormalization(TestCase):
         self.assertTrue(os.path.exists(EBU_DYNAMIC_RES))
 
 
-    # @todo need a peak clipping test
-    # @todo need a peak max volume test
-
-
-    def test_peak_normalize_file(self):
-        '''
-        @brief Tests peak normalize audio file level.
-        '''
-
-        normalization.peak_normalize_file(PEAK_SRC, show_spinner=False)
-        self.assertTrue(os.path.exists(PEAK_RES))
-
-
-    # @todo need a passing rms test
-    # @todo need a rms max volume test
-
-
-    def test_rms_normalize_file_clipping(self):
-        '''
-        @brief Tests rms normalize audio file level would clip.
-
-        @details This test will fail if ran with pytest.
-        '''
-
-        module = f"{normalization.__module__}"
-        logger = logging.getLogger(module)
-
-        with self.assertLogs(logger, level=logging.WARNING) as cm:
-            normalization.rms_normalize_file(RMS_CLIPPING_SRC, show_spinner=False)
-
-        self.assertIn(RMS_CLIPPING_RES, cm.output[0])
-
-
     def test_get_bit_rate(self):
         '''
         @brief Tests getting bit rate.
         '''
 
         bit_rate = normalization.get_bit_rate(BIT_SRC)
-        self.assertEqual(BIT_RES, bit_rate)
+
+        res_bitrate = math.floor(bit_rate / 1000)
+        exp_bitrate = math.floor(BIT_RES / 1000)
+
+        self.assertEqual(res_bitrate, exp_bitrate)
+
+
+    # @todo add get_sample_rate tests
+    # can have IndexError
+    # can have JSONDecodeError
 
 
     def test_get_sample_rate(self):
@@ -148,7 +112,79 @@ class TestAudioNormalization(TestCase):
 
         volumes = normalization.get_volume_info(VOL_SRC)
         self.maxDiff = None
-        self.assertDictEqual(volumes, VOLUME_INFO_RES)
+        self.assertDictEqual(volumes, VOL_INFO_RES)
+
+
+    # @todo add __loudnorn_json_parse tests
+    # can have JSONDecodeError
+    # can have JSONOutputError
+
+
+    def test_peak_normalize_file(self):
+        '''
+        @brief Tests peak normalize audio file level.
+        '''
+
+        normalization.peak_normalize_file(PEAK_SRC, show_spinner=False)
+        self.assertTrue(os.path.exists(PEAK_RES))
+
+
+    @unittest.skip("Need a file that will max volume")
+    def test_peak_normalize_file_max_volume(self):
+        '''
+        @brief Tests peak normalize audio file level would have mav volume.
+        '''
+
+        pass
+
+
+    @unittest.skip("Need a file that will clip")
+    def test_peak_normalize_file_clipping(self):
+        '''
+        @brief Tests rms normalize audio file level would clip.
+        '''
+
+        # module = f"{normalization.__module__}"
+        # logger = logging.getLogger(module)
+
+        # with self.assertLogs(logger, level=logging.WARNING) as cm:
+        #     normalization.peak_normalize_file(PEAK_CLIPPING_SRC, show_spinner=False)
+
+        # self.assertIn(PEAK_CLIPPING_RES, cm.output[0])
+        pass
+
+
+    @unittest.skip("Need a file that won't fail")
+    def test_rms_normalize_file(self):
+        '''
+        @brief Tests rms normalize audio file level.
+        '''
+
+        normalization.rms_normalize_file(RMS_SRC, show_spinner=False)
+        self.assertTrue(os.path.exists(RMS_RES))
+
+
+    @unittest.skip("Need a file that will max volume")
+    def test_rms_normalize_max_volume(self):
+        '''
+        @brief Tests rms normalize audio file level would have mav volume.
+        '''
+
+        pass
+
+
+    def test_rms_normalize_file_clipping(self):
+        '''
+        @brief Tests rms normalize audio file level would clip.
+        '''
+
+        module = f"{normalization.__module__}"
+        logger = logging.getLogger(module)
+
+        with self.assertLogs(logger, level=logging.WARNING) as cm:
+            normalization.rms_normalize_file(RMS_CLIPPING_SRC, show_spinner=False)
+
+        self.assertIn(RMS_CLIPPING_RES, cm.output[0])
 
 
 def get_method_names(cls):
