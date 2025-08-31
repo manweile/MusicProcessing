3e9a5340db612b139a495da4ff758212eadb7ebe
manweile <manweile@ualberta.ca>
2025-08-30 18:00:43 -0600

    update function call

--- src/audio_info/audio_metadata.py
+++ src/audio_info/audio_metadata.py
@@ -48,7 +48,7 @@ gc.enable()
 
 logger = logging.getLogger(__name__)
 basename = os.path.basename(__file__)
-add_module_handler(logger, basename, logging.DEBUG, propagate=True)
+add_module_handler(logger, basename)
 
 directory = DirectoryProcessing()
 normalization = AudioNormalization()
