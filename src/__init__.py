ca198b5c1104834ace5a3f45a14cac930bbc0875
manweile <manweile@ualberta.ca>
2025-09-12 11:16:29 -0600

    add doc comments

--- src/__init__.py
+++ src/__init__.py
@@ -6,6 +6,7 @@
 # standard modules
 import logging
 import os
+from logging import DEBUG, FileHandler, Formatter
 
 # local module constants
 from src.generated_files import GENERATED_FILES
@@ -124,7 +125,7 @@ RESULT_EXT = ".txt"
 UTF8 = "utf-8"
 
 ## @var WMA_EXT
-# @brief wma file typer extension
+# @brief wma file type extension
 # @details use this when needing just this file type extension and not file list of valid extensions
 WMA_EXT = ".wma"
 
@@ -187,24 +188,57 @@ Additionally, there will be level based loggers.
 The debug logger will not have a filter, making it the master log repository.
 The info through critical loggers will be filtered to only accept log records of their level.
 '''
+## @var handler
+# @brief log file handler
+# @details creates log file handler for log level
+handler = FileHandler
 
+## @var handler_formatter
+# @brief logging formatter
+# @details the logging format for log level
+handler_formatter = Formatter
+
+## @ var handler_level
+# @brief handler log level
+# @details the log level for handler
+handler_level = int()
+
+## @var levels
+# @brief logging levels for project
+# @details specifies what log levels will generate logs
 levels = ("DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL")
+
+## @var level_log_file
+# @brief log file name
+# @details creates log file name from log level
+level_log_file = str()
+
+## @var level_log_path
+# @brief log file path
+# @details creates log file path for a log file
+level_log_path = str()
+
+## @var log_path
+# @brief path to log files
+# @details specifies fixed log path for project
 log_path = os.path.join(GENERATED_FILES, LOG_DIR)
 
-# use name of package so logger is parent to loggers in other modules in same package
+## @var src_logger
+# @brief logger for package
+# @details uses name of package so logger is parent to loggers in other modules in same package
 src_logger = logging.getLogger(__name__)
 
 # override the default logging level WARN to lowest level so we can log all levels
-src_logger.setLevel(logging.DEBUG)
+src_logger.setLevel(DEBUG)
 
 # add a handler for each level and attach it to the single logger at top of hierarchy
 for level in levels:
     level_log_file = f"{level.lower()}{LOG_EXT}"
     level_log_path = os.path.join(log_path, level_log_file)
 
-    handler = logging.FileHandler(level_log_path, mode="a", encoding=UTF8)
+    handler = FileHandler(level_log_path, mode="a", encoding=UTF8)
 
-    handler_formatter = logging.Formatter(ERROR_LOG_FORMAT)
+    handler_formatter = Formatter(ERROR_LOG_FORMAT)
     handler.setFormatter(handler_formatter)
 
     handler_level = getattr(logging, level)
@@ -215,7 +249,7 @@ for level in levels:
     src_logger.addHandler(handler)
 
 
-def add_module_handler(logger, basename, level=logging.DEBUG, format=ERROR_LOG_FORMAT, propagate=True):
+def add_module_handler(logger, basename, level=DEBUG, format=ERROR_LOG_FORMAT, propagate=True):
     '''
     @brief Adds FileHandler to a logger.
 
@@ -223,21 +257,22 @@ def add_module_handler(logger, basename, level=logging.DEBUG, format=ERROR_LOG_F
     @details basename is expected to be defined by __file__ dunder in calling module.
 
     @param logger (Logger) Logger instance for a module.
-    @param basename {str} Set file handler log file name for logger.
-    @param level {int} Optional, sets logger & file handler logging level.
-    @param format {str} Optional, sets file handler logging format.
-    @param propagate {bool} Optional, sets logger to propagation to root logger.
+    @param basename {str} File handler log file name for logger.
+    @param level {int} Optional, Logger & file handler logging level.
+    @param format {str} Optional, File handler logging format.
+    @param propagate {bool} Optional, Logger propagation to root logger.
     '''
 
     logger.setLevel(level)
 
+    # log files always go to fixed location
     stem = os.path.splitext(basename)[0]
     log_file = stem + LOG_EXT
     log_path = os.path.join(GENERATED_FILES, LOG_DIR, log_file)
 
-    formatter = logging.Formatter(format)
+    formatter = Formatter(format)
 
-    handler = logging.FileHandler(log_path, encoding=UTF8)
+    handler = FileHandler(log_path, encoding=UTF8)
     handler.setFormatter(formatter)
     handler.setLevel(level)
 
