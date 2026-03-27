8818791868425c2c030a9061e8e7ab37f9c541b2
manweile <manweile@ualberta.ca>
2026-03-27 15:39:49 -0600

    doxygen for test_gui.py

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -1,23 +1,39 @@
-"""
+'''
 @file test_gui.py
 @brief Simple GUI smoke tests for wxPython support.
-"""
 
+@author Gerald Manweiler
+@copyright @showdate "%Y" GWN Software. All rights reserved.
+'''
+
+# standard modules
 import unittest
 
+# third party modules
 try:
     import wx
 except ImportError:  # pragma: no cover
     wx = None
 
+# local module classes
 from src.gui.wx_app import MainFrame, run_gui
 
 
 class TestGuiSmoke(unittest.TestCase):
-    '''Basic checks for GUI module.'''
+    '''
+    @briefBasic checks for GUI module.
+
+    @details These tests are not meant to be exhaustive, but rather to catch basic issues with the GUI module and its dependencies.
+    '''
 
     @unittest.skipIf(wx is None, "wxPython is not installed")
     def test_mainframe_can_instantiate(self):
+        '''
+        @brief Test that the MainFrame can be instantiated without errors.
+
+        @details This test will fail if there are any issues with the MainFrame class or its dependencies that prevent it from being created.
+        '''
+
         app = wx.App(False)
         frame = MainFrame(None)
         self.assertIsInstance(frame, wx.Frame)
@@ -26,4 +42,10 @@ class TestGuiSmoke(unittest.TestCase):
 
     @unittest.skipIf(wx is None, "wxPython is not installed")
     def test_run_gui_callable(self):
+        '''
+        @brief Test that the run_gui function is callable.
+
+        @details This test will fail if there are any issues with the run_gui function or its dependencies that prevent it from being called.
+        '''
+
         self.assertTrue(callable(run_gui))
